9930a9344e6739649b18505abd0e13f3f9eaa6c1
miro <jarbasai@mailfence.com>
2024-06-21 19:44:44 +0100

    feat/autoconfigure script

--- ovos_media_plugin_spotify/autoconfigure.py
+++ ovos_media_plugin_spotify/autoconfigure.py
@@ -7,7 +7,7 @@ from ovos_media_plugin_spotify.spotify_client import SpotifyClient
 
 def main():
     print(
-        """This script will auto configure ALL spotify devices under your mycroft.conf
+        """This script will auto configure spotify devices under your mycroft.conf
         
         SPOTIFY PREMIUM is required!
         
@@ -15,34 +15,49 @@ def main():
         """)
     cfg = MycroftUserConfig()
     spotify = SpotifyClient()
-    for d in spotify.devices:
-        print(f"Found device: {d['name']}")
+    devices = [d['name'] for d in spotify.devices]
+    if not devices:
+        print("ERROR: no spotify devices found in this account")
+        exit(1)
 
-        if "Audio" not in cfg:
-            cfg["Audio"] = {}
-        if "backends" not in cfg["Audio"]:
-            cfg["Audio"]["backends"] = {}
+    print(f"Found devices: {devices}")
+    if len(devices) == 1:
+        default = 0
+    else:
+        for idx, d in enumerate(devices):
+            print(f"{idx} - {d}")
+        default = int(input("select default spotify device:"))
+
+    for idx, d in enumerate(devices):
         if "media" not in cfg:
             cfg["media"] = {}
         if "audio_players" not in cfg["media"]:
             cfg["media"]["audio_players"] = {}
 
-        cfg["Audio"]["backends"]["spotify-" + d['name']] = {
-            "type": "ovos_spotify",
-            "identifier": d['name'],
-            "active": True
-        }
-        cfg["media"]["audio_players"]["spotify-" + d['name']] = {
+        if idx == default:
+            if "Audio" not in cfg:
+                cfg["Audio"] = {}
+            if "backends" not in cfg["Audio"]:
+                cfg["Audio"]["backends"] = {}
+            cfg["Audio"]["backends"]["spotify-" + d] = {
+                "type": "ovos_spotify",
+                "identifier": d,
+                "active": True
+            }
+
+        cfg["media"]["audio_players"]["spotify-" + d] = {
             "module": "ovos-media-audio-plugin-spotify",
-            "identifier": d['name'],
-            "aliases": [d['name']],
+            "identifier": d,
+            "aliases": [d.replace("-", " ")],
             "active": True
         }
     cfg.store()
 
     print("\nmycroft.conf updated!")
+
     print("\n# Legacy Audio Service:")
     pprint(cfg["Audio"])
+
     print("\n# ovos-media Service:")
     pprint(cfg["media"])
 
